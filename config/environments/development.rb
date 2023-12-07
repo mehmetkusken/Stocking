e8338ddd0f6fefff01368b0a90b358bc03a450d4
Mehmet <mehmetkusken@gmail.com>
2023-12-07 14:13:30 +0300

    added production new host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://floating-plateau-54978-1ac34a739ba0.herokuapp.com', port: 3000 }
 end
